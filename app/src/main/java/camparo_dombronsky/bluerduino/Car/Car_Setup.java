f3313643c39373c9450f4a34c4fca906460e9efc
buddyff <rodrigocamparo@gmail.com>
2016-04-06 16:10:13 -0300

    Refactoring del codigo para la conexion Bluetooth

--- app/src/main/java/camparo_dombronsky/bluerduino/Car/Car_Setup.java
+++ app/src/main/java/camparo_dombronsky/bluerduino/Car/Car_Setup.java
@@ -1,22 +1,19 @@
 package camparo_dombronsky.bluerduino.Car;
 
-import android.bluetooth.BluetoothSocket;
-import android.os.Bundle;
-
 import camparo_dombronsky.bluerduino.R;
+import camparo_dombronsky.bluerduino.Utils.Connect2Arduino;
 
 import java.io.IOException;
-import java.io.OutputStream;
 import java.util.Set;
 import java.util.UUID;
 
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 import android.content.Intent;
+import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
 import android.widget.Toast;
 
 
@@ -27,15 +24,10 @@ public class Car_Setup extends AppCompatActivity {
 
     // Objects for the Bluetooth Connection
     private  BluetoothAdapter btAdapter;
-    private OutputStream outStream;
-    private BluetoothDevice btDevice;
 
     //Thread
-    Connect2Arduino  hilo;
+    Connect2Arduino connect2arduino;
 
-    public static String EXTRA_DEVICE_ADDRESS;
-    private static final String ARDUINO_MAC = "98:D3:35:00:98:52";
-    private static final UUID ARDUINO_UUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,89 +35,54 @@ public class Car_Setup extends AppCompatActivity {
         setContentView(R.layout.car_setup);
 
         start_car = (Button) findViewById(R.id.btn_startCar);
+        start_car.setEnabled(false);
         start_car.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 Intent intent = new Intent(Car_Setup.this, Car_Activity.class);
                 startActivity(intent);
             }
         });
-
-
-
     }
 
     @Override
     public void onResume() {
-        super.onResume();
+        try {
+            super.onResume();
+            //It is best to check BT status at onResume in case something has changed while app was paused etc
+            if (checkBTState()) {
+                connect2arduino = new Connect2Arduino(btAdapter, this);
 
-        //It is best to check BT status at onResume in case something has changed while app was paused etc
-        checkBTState();
-
-
-
-        // Get a set of currently paired devices
-        Set<BluetoothDevice> pairedDevices = btAdapter.getBondedDevices();
-
-        // Look for the Arduino Bluetooth Adapter
-        if (pairedDevices.size() > 0) {
-            boolean arduino_founded = false;
-            for (BluetoothDevice device : pairedDevices) {
-                if (device.getAddress().equals(ARDUINO_MAC)) {
-                    //connect2arduino(device.getAddress());
-                    System.out.println(device.getAddress());
-                    hilo = new Connect2Arduino(device);
-                    hilo.start();
-                    arduino_founded = true;
-                    break;
+                if (connect2arduino.getSocket() != null) {
+                    connect2arduino.start();
+                    if (connect2arduino.getSocket().isConnected())
+                        start_car.setEnabled(true);
                 }
             }
+        }
+        catch (IOException e){
+            Toast.makeText(getBaseContext(), "No se pudo establecer conexión Bluetooth", Toast.LENGTH_SHORT).show();
+            e.printStackTrace();
         }
     }
 
     @Override
     public void onPause() {
         super.onPause();
-        System.out.println("PAUSA");
-        //Close BT socket to device
-        try {
-            if (hilo != null ) {
-                if (hilo.getSocket() != null) {
-                    System.out.println("CIERRO SOCKET DE PAUSA");
-                    hilo.getSocket().close();
-                }
-            }
-        } catch (IOException e2) {
-            Toast.makeText(getBaseContext(), "ERROR - No se pudo cerrar el Socket Bluetooth", Toast.LENGTH_SHORT).show();
-        }
     }
 
     @Override
     public void onStop() {
         super.onStop();
-        System.out.println("STOP");
-        //Close BT socket to device
-        try {
-            if (hilo != null) {
-                if (hilo.getSocket() != null) {
-                    System.out.println("CIERRO SOCKET DE STOP");
-                    hilo.getSocket().close();
-                }
-            }
-        }catch (IOException e2) {
-            Toast.makeText(getBaseContext(), "ERROR - No se pudo cerrar el Socket Bluetooth", Toast.LENGTH_SHORT).show();
-        }
     }
 
     @Override
     public void onDestroy() {
         super.onDestroy();
-        System.out.println("DESTROY");
         //Close BT socket to device
         try {
-            if (hilo != null ) {
-                if (hilo.getSocket() != null) {
-                    System.out.println("CIERRO SOCKET DE DESTROY");
-                    hilo.getSocket().close();
+            if (connect2arduino != null ) {
+                if (connect2arduino.getSocket() != null) {
+                    connect2arduino.getSocket().close();
                 }
             }
         }
@@ -136,8 +93,7 @@ public class Car_Setup extends AppCompatActivity {
 
     //method to check if the device has Bluetooth and if it is on.
     //Prompts the user to turn it on if it is off
-    private void checkBTState() {
-
+    private boolean checkBTState() {
         // Check if the device has Bluetooth and that it is turned on
         btAdapter = BluetoothAdapter.getDefaultAdapter();
         if (btAdapter == null) {
@@ -148,70 +104,14 @@ public class Car_Setup extends AppCompatActivity {
                 //Prompt user to turn on Bluetooth
                 Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
                 startActivityForResult(enableBtIntent, 1);
+                return false;
             }
         }
+        return true;
     }
 
 
-    // Method to send data
-    private void sendData(String message) {
-        byte[] msgBuffer = message.getBytes();
-        try {
-            //attempt to place data on the outstream to the BT device
-            outStream.write(msgBuffer);
-        } catch (IOException e) {
-            //if the sending fails this is most likely because device is no longer there
-            Toast.makeText(getBaseContext(), "ERROR - Dispositivo no encontrado", Toast.LENGTH_SHORT).show();
-            // finish();
-        }
-    }
 
-
-    class Connect2Arduino extends Thread {
-        private final BluetoothSocket socket;
-        private final BluetoothDevice device;
-
-        public Connect2Arduino(BluetoothDevice device) {
-            BluetoothSocket tmp = null;
-            this.device = device;
-
-            // Get a BluetoothSocket to connect with the given BluetoothDevice
-            try {
-
-                tmp = device.createRfcommSocketToServiceRecord(ARDUINO_UUID);
-            } catch (IOException e) { }
-            socket = tmp;
-        }
-
-        public void run() {
-            // Cancel discovery because it will slow down the connection
-            btAdapter.cancelDiscovery();
-
-            try {
-                // Connect the device through the socket. This will block
-                // until it succeeds or throws an exception
-                socket.connect();
-            } catch (IOException connectException) {
-                // Unable to connect; close the socket and get out
-                try {
-                    socket.close();
-                } catch (IOException closeException) { }
-                return;
-            }
-        }
-
-        /** Will cancel an in-progress connection, and close the socket */
-        public void cancel() {
-            try {
-                System.out.println("fin hilo");
-                socket.close();
-            } catch (IOException e) { }
-        }
-
-        public BluetoothSocket getSocket(){
-            return socket;
-        }
-    }
 }
 
 
---
+++ app/src/main/java/camparo_dombronsky/bluerduino/Utils/Connect2Arduino.java
@@ -0,0 +1,113 @@
+package camparo_dombronsky.bluerduino.Utils;
+
+
+import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
+import android.content.Context;
+import android.os.Looper;
+import android.os.Message;
+import android.widget.Button;
+import android.widget.Toast;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.Set;
+import java.util.UUID;
+import java.util.logging.Handler;
+
+import camparo_dombronsky.bluerduino.Car.Car_Setup;
+import camparo_dombronsky.bluerduino.R;
+
+public class Connect2Arduino extends Thread {
+    private BluetoothSocket socket;
+    private BluetoothDevice device;
+    private BluetoothAdapter adapter;
+    private OutputStream outStream;
+    private Activity callerActivity;
+
+    private static final String ARDUINO_MAC = "98:D3:35:00:98:52";
+    private static final UUID ARDUINO_UUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB");
+
+    public Connect2Arduino(BluetoothAdapter adap, Activity act) throws IOException {
+        adapter = adap;
+        callerActivity = act;
+
+        // Get a set of currently paired devices
+        Set<BluetoothDevice> pairedDevices = adapter.getBondedDevices();
+
+        // Look for the Arduino Bluetooth Device
+        if (pairedDevices.size() > 0) {
+            for (BluetoothDevice paired_device : pairedDevices) {
+                if (paired_device.getAddress().equals(ARDUINO_MAC)) {
+                   device = paired_device;
+                    break;
+                }
+            }
+        }
+        if (device != null)
+            socket = device.createRfcommSocketToServiceRecord(ARDUINO_UUID);
+        else{
+            callerActivity.runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    Toast.makeText(callerActivity, "El dispositivo Bluetooth del Arduino no se encuentra emparejado", Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
+    }
+
+    public void run() {
+        // Cancel discovery because it will slow down the connection
+        adapter.cancelDiscovery();
+
+        try {
+            // Connect the device through the socket. This will block
+            // until it succeeds or throws an exception
+            socket.connect();
+            outStream = socket.getOutputStream();
+            callerActivity.runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    Toast.makeText(callerActivity, "Conexion con Arduino establecida correctamente", Toast.LENGTH_SHORT).show();
+                }
+                });
+
+
+        }
+        catch (IOException connectException) {
+            // Unable to connect; close the socket,toast a message and get out
+            try {
+                socket.close();
+                callerActivity.runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Toast.makeText(callerActivity, "No se pudo establecer conexion con el Arduino", Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+            catch (IOException closeException) { }
+            return;
+        }
+    }
+
+    /** Will cancel an in-progress connection, and close the socket */
+    public void cancel() {
+        try {
+            System.out.println("fin hilo");
+            socket.close();
+        } catch (IOException e) { }
+    }
+
+    // Method to send data
+    public void sendData(String message) throws IOException {
+        byte[] msgBuffer = message.getBytes();
+        //attempt to place data on the outstream to the BT device
+        outStream.write(msgBuffer);
+    }
+
+    public BluetoothSocket getSocket(){return socket;}
+
+    public OutputStream getOutStream(){return outStream;}
+}
